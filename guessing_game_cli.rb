8cac0a8e7a56f3f4b0d293c1947cec11a8b67373
Melissa Lawrence <mlawrence537@gmail.com>
2021-02-12 15:25:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,9 +6,20 @@ def guess_prompt
   puts "Guess a number between 1 and 6"
 end
 
+def user_guess
+  gets.chomp
+end
+
 
 
 def run_guessing_game
-  gen_number
+  random = gen_number
   guess_prompt
+  guess = user_guess
+  if guess == random
+    puts "You guessed the correct number!"
+    elsif guess == "exit"
+    puts "Goodbye!"
+  else puts "Sorry! The computer guessed #{random}."
+end
 end
